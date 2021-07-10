d3fa331a1838ce1731d95763a6be97fbe86c5a81
KoutaKawase <suhrkawase@gmail.com>
2021-07-10 12:25:25 +0900

    🚧 とりあえずcmd履歴だけ取得できた

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+extern crate yaml_rust;
 use std::{
     env,
     fs::File,
-    io::{self, BufRead, BufReader},
+    io::{self, BufRead, BufReader, Read},
     path::{Path, PathBuf},
 };
+use yaml_rust::yaml;
 
 pub enum Env {
     Production,
@@ -19,25 +21,48 @@ fn get_history_file_path(env: Env) -> PathBuf {
     path
 }
 
+fn print_indent(indent: usize) {
+    for _ in 0..indent {
+        print!("    ");
+    }
+}
+
+fn dump_node(doc: &yaml::Yaml, indent: usize) {
+    match *doc {
+        yaml::Yaml::Array(ref v) => {
+            for x in v {
+                dump_node(x, indent + 1);
+            }
+        }
+        yaml::Yaml::Hash(ref h) => {
+            for (k, v) in h {
+                //print_indent(indent);
+                if k.as_str().unwrap() == "cmd" {
+                    dump_node(v, indent + 1);
+                }
+            }
+        }
+        _ => {
+            //print_indent(indent);
+            println!("{:?}", doc);
+        }
+    }
+}
+
 fn main() -> io::Result<()> {
     let history_file_path = get_history_file_path(Env::Dev);
 
     let file = File::open(history_file_path).unwrap();
     let mut reader = BufReader::new(file);
+    let mut buf = String::new();
 
-    for _ in 0..5 {
-        let mut buf = String::new();
-        const EOF: usize = 0;
-        match reader.read_line(&mut buf) {
-            Ok(EOF) => {
-                println!("END");
-                break;
-            }
-            Ok(_) => (),
-            Err(why) => panic!("Something wrong!! {}", why.to_string()),
-        }
-        println!("{}", buf);
-    }
+    reader.read_to_string(&mut buf).unwrap();
+
+    let histories = yaml::YamlLoader::load_from_str(&buf).unwrap();
+    let history_yaml = &histories[0];
+
+    println!("---");
+    dump_node(&history_yaml, 0);
 
     Ok(())
 }
